7363b17267a861aa577739a27ed2937c215b9b92
Luke Skywalker <joshua.mobley.jm@gmail.com>
2019-01-10 16:05:17 -0700

    added heroku

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: node logic.js
--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,28 +1,49 @@
-
-$('#upload').change(function(e){
+$('#upload').change(function (e) {
     var reader = new FileReader();
 
     reader.readAsArrayBuffer(e.target.files[0]);
 
-    reader.onload = function(e) {
+    reader.onload = function (e) {
         var data = new Uint8Array(reader.result);
-        var wb = XLSX.read(data,{type: 'array'});
+        var wb = XLSX.read(data, { type: 'array' });
+        var first_sheet_name = wb.SheetNames[0];
 
-        var htmlstr = XLSX.write(wb,{sheet: 'Cabinet Detail (sub status) - v', type: 'binary', bookType: 'html'});
+        var htmlstr = XLSX.write(wb, { sheet: first_sheet_name, type: 'binary', bookType: 'html' });
         $('#sample').append(htmlstr);
+
+        console.log(htmlstr);
     }
 });
 
-$('#drop').change(function(e){
-    var reader = new FileReader();
+// var XLSX = require('xlsx');
+// var workbook = XLSX.readFile('test.xlsx');
+// var sheet_name_list = workbook.SheetNames;
+// sheet_name_list.forEach(function(y) {
+//     var worksheet = workbook.Sheets[y];
+//     var headers = {};
+//     var data = [];
+//     for(z in worksheet) {
+//         if(z[0] === '!') continue;
+//         //parse out the column, row, and value
+//         var tt = 0;
+//         for (var i = 0; i < z.length; i++) {
+//             if (!isNaN(z[i])) {
+//                 tt = i;
+//                 break;
+//             }
+//         };
+//         var col = z.substring(0,tt);
+//         var row = parseInt(z.substring(tt));
+//         var value = worksheet[z].v;
 
-    reader.readAsArrayBuffer(e.target.files[0]);
+//         //store header names
+//         if(row == 1 && value) {
+//             headers[col] = value;
+//             continue;
+//         }
 
-    reader.onload = function(e) {
-        var data = new Uint8Array(reader.result);
-        var wb = XLSX.read(data,{type: 'array'});
-
-        var htmlstr = XLSX.write(wb,{sheet: 'Cabinet Detail (sub status) - v', type: 'binary', bookType: 'html'});
-        $('#sample').append(htmlstr);
-    }
-});
+//         if(!data[row]) data[row]={};
+//         data[row][headers[col]] = value;
+//     }
+//     console.log(data);
+// });
