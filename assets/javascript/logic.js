9b15736e5e0919ead0a9011ce218f7ca26aeed59
Luke Skywalker <joshua.mobley.jm@gmail.com>
2019-01-11 18:07:01 -0700

    updated formatting and added in comments

--- assets/javascript/logic.js
+++ assets/javascript/logic.js
@@ -1,20 +1,30 @@
 
 const rABS = true; // true: readAsBinaryString ; false: readAsArrayBuffer
 
+// main function loader for when file is selected for upload
 function handleFile(e) {
+
+    // defines variable for uploaded file
     const files = e.target.files, f = files[0];
     const reader = new FileReader();
+
+    // Function that determines what to do with uploaded file
     reader.onload = function (e) {
         const data = e.target.result;
 
         if (!rABS) data = new Uint8Array(data);
-        const workbook = XLSX.read(data, { type: rABS ? 'binary' : 'array' });
 
+        // variable to pull data and pasrse through xlsx.js library
+        const workbook = XLSX.read(data, { type: rABS ? 'binary' : 'array' });
         const first_sheet_name = workbook.SheetNames[0];
-        /* Get worksheet */
         const worksheet = workbook.Sheets[first_sheet_name];
-        console.log(XLSX.utils.sheet_to_json(worksheet, { raw: true }));
+
+        // end result. uploaded file formated into json
+        const jsonSheet = XLSX.utils.sheet_to_json(worksheet, { raw: true });
+        console.log(jsonSheet);
+        
     };
+
     if (rABS) reader.readAsBinaryString(f); else reader.readAsArrayBuffer(f);
 }
 
