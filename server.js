2f3c9c64c0ecd6ac63cee3a730129eed42ba5e08
Joshua Mobley <joshua_mobley@apple.com>
2019-02-05 11:00:09 -0700

    changed all files to match clg-tool

--- server.js
+++ server.js
@@ -89,7 +89,7 @@ app.post("/api/characters", function(req, res) {
 
   // Using a RegEx Pattern to remove spaces from newCharacter
   // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
-  newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
+  // newcharacter.routeName = newcharacter.name.replace(/\s+/g, "").toLowerCase();
 
   console.log(newcharacter);
 
